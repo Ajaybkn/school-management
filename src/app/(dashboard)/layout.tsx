986f90aaf2a5c727a9efc4472724616efb6d59be
Ajay Singh Khangarot <110377839+Ajaybkn@users.noreply.github.com>
2024-10-03 13:01:38 +0530

    update

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import Menu from "@/components/Menu";
 import Image from "next/image";
 import Link from "next/link";
 export default function DashboardLayout({
@@ -8,7 +9,7 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* left */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 text-black p-4">
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]  text-black p-4">
         <Link
           href="/"
           className="flex items-center justify-center lg:justify-center gap-2"
@@ -16,9 +17,10 @@ export default function DashboardLayout({
           <Image src="/logo.png" alt="logo" width={32} height={32} />
           <span className="hidden lg:block uppercase">ajay's academy</span>
         </Link>
+        <Menu />
       </div>
       {/* right */}
-      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-yellow-200 text-black">
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%]  text-black">
         right
       </div>
       {children}
